6a424b79a7f381399002d43933f49cfd91cfb76a
Javesbalaaba1014 <146338106+Javesbalaaba1014@users.noreply.github.com>
2025-01-04 10:28:30 +0800

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import { WorkExperience } from './WorcCard'
 import TechStack from './tech-stack'
 import { ParticleEffect } from './components/ParticleEffect'
-
+import Contact from './Contact'
 function App() {
   return (
     <div className="bg-[#0a0118] w-full">
@@ -14,6 +14,7 @@ function App() {
       <Hero />
       <WorkExperience />
       <TechStack />
+      <Contact />
     </div>
   )
 }
